926810f311230a070bba3db99b1fd114727f7bf2
Celine Fong <celinefong4@gmail.com>
2020-02-08 14:24:55 -0700

    cursor movement updated, push functionality added

--- part1.cpp
+++ part1.cpp
@@ -49,9 +49,13 @@
 #define LON_WEST -113686521
 #define LON_EAST -113334961
 
+// declare map
+lcd_image_t yegImage = {"yeg-big.lcd", MAP_WIDTH, MAP_HEIGHT};
+
 // thresholds for the joystick
 #define JOY_CENTER   512
 #define JOY_DEADZONE 64
+#define BUFFER 400
 #define CURSOR_SIZE 9
 
 MCUFRIEND_kbv tft;
@@ -86,7 +90,23 @@ RestDist rest_dist[NUM_RESTAURANTS];
 // selectedRest needs to change with joystick movement
 int selectedRest = 0;
 
+// Mode 0
 
+// current cursor position on the display
+int cursorX, cursorY;
+
+// previous cursor position on the display
+int prevX, prevY;
+
+// coordinates to draw middle of map
+int yegMiddleX, yegMiddleY;
+
+// coordinates of current point on map
+int yegCurrX, yegCurrY;
+
+// forward declaration for redrawing cursor
+void redrawCursor(uint16_t colour);
+ 
 void setup() {
   	init();
   	pinMode(JOYSTICK_SEL, INPUT);
@@ -97,8 +117,12 @@ void setup() {
   	uint16_t ID = tft.readID();
   	tft.begin(ID);
 
-  	tft.fillScreen(TFT_BLACK);
-  	tft.setRotation(1);
+  	// SD card initialization of SD card reads
+    Serial.print("Initializing SD card...");
+    if (!SD.begin(SD_CS)) {
+      Serial.println("failed! Is it inserted properly?");
+      while (true) {}
+    }
 
   	// SD card initialization for raw reads
   	Serial.print("Initializing SPI communication for raw reads...");
@@ -109,6 +133,21 @@ void setup() {
   	else {
   		Serial.println("OK!");
   	}
+
+  	tft.fillScreen(TFT_BLACK);
+  	tft.setRotation(1);
+
+  	// set initial map patch to be drawn
+    yegMiddleX = MAP_WIDTH/2 - MAP_DISP_WIDTH/2;
+    yegMiddleY = MAP_HEIGHT/2 - MAP_DISP_HEIGHT/2;
+
+    yegCurrX = yegMiddleX;
+    yegCurrY = yegMiddleY;
+}
+
+void redrawCursor(uint16_t colour) {
+  tft.fillRect(cursorX - CURSOR_SIZE/2, cursorY - CURSOR_SIZE/2,
+               CURSOR_SIZE, CURSOR_SIZE, colour);
 }
 
 // These functions convert between x/y map position and lat/lon
@@ -252,10 +291,9 @@ void moveHighlight(RestDist* restArray, int x) {
 }
 
 // Processes Joystick input and controls the cursor accordingly.
-bool joystick() {
+void joystickMode1() {
 	int prevRest = selectedRest;
 	int yVal = analogRead(JOYSTICK_VERT);
-	int buttonVal = digitalRead(JOYSTICK_SEL);
 	selectedRest = constrain(selectedRest, 0, 20);
 
 	// joystick up
@@ -274,40 +312,15 @@ bool joystick() {
 			moveHighlight(rest_dist, prevRest);
 		}
 	}
-	if (buttonVal == LOW) {
-		return true;
-	} else {
-		return false;
-	}
 }
 
+// forward declaration of mode0
+void mode0();
 
-int main() {
-	setup();
-	// when the user presses the joystick, enter mode 1
-	// display list of restaurants that are closest to the chosen position
-		// 21 restaurants
-		// text size of 2
-		// sort by Manhattan distance
-		// d((x1,y1),(x2,y2)) = |x1 - x2| + |y1 - y2|
-	// user can use joystick to choose a restaurant (highlight block)
-	// when user pushes the button again, the program will go back to mode 0
-	// but with the selected restaurant at the centre
-
-	// IMPLEMENT THIS CODE WHEN JOYSTICK BUTTON IS PRESSED
-	// xPos, yPos = position of cursor when pressed
-	int cursorX, cursorY;
-	tft.setTextWrap(false);
-	tft.setTextSize(2);
-	// values of cursorX and cursorY should change,
-	// should probably be global variables but I used them
-	// here for testing
-	cursorX = 300;
-	cursorY = 200;
-
+void mode1() {
 	// 1. load restaurant data into array of RestDist structs
 		// calculate manhattan distance for each restaurant
-	getRestDist(rest_dist, cursorX, cursorY);
+	getRestDist(rest_dist, yegCurrX + cursorX, yegCurrY + cursorY);
 	Serial.println("Created RestDist");
 	// 2. sort array of RestDist structs by distance
 	isort(rest_dist, NUM_RESTAURANTS);
@@ -317,15 +330,195 @@ int main() {
 	Serial.println("Displayed");
 	// 4. if joystick is moved, scroll through list
 	// 5. if joystick is pressed, go back to map display
-	bool pressed = false;
-	while (!pressed) {
-		// will break out of this loop when the joystick
-		// button is pressed
-		pressed = joystick();
+	while (digitalRead(JOYSTICK_SEL) == HIGH) {
+		joystickMode1();
 	}
+	mode0();
+}
+
+// functions needed for mode0
+
+// the following functions adapted from weekly exercise #1
+
+// redraws portion of map that cursor has just left to prevent black trail
+void redrawMap() {
+    int adjustX = prevX - CURSOR_SIZE/2;
+    int adjustY = prevY - CURSOR_SIZE/2;
+    int mapPosX = yegCurrX + adjustX;
+    int mapPosY = yegCurrY + adjustY;
+    lcd_image_draw(&yegImage, &tft, mapPosX, mapPosY,
+                   adjustX, adjustY,
+                   CURSOR_SIZE, CURSOR_SIZE);
+}
+
+// handle what to do when cursor hits map edge
+void drawNextPatchRight() {
+	yegCurrX = yegCurrX + MAP_DISP_WIDTH;
+	yegCurrX = constrain(yegCurrX, 0, MAP_WIDTH - MAP_DISP_WIDTH);
+	lcd_image_draw(&yegImage, &tft, 
+				   yegCurrX, yegCurrY,
+				   0, 0,
+				   MAP_DISP_WIDTH, MAP_DISP_HEIGHT);
+}
+
+void drawNextPatchLeft() {
+	yegCurrX = yegCurrX - MAP_DISP_WIDTH;
+	yegCurrX = constrain(yegCurrX, 0, MAP_WIDTH - MAP_DISP_WIDTH);
+	lcd_image_draw(&yegImage, &tft, 
+				   yegCurrX, yegCurrY,
+				   0, 0,
+				   MAP_DISP_WIDTH, MAP_DISP_HEIGHT);	
+}
+
+void drawNextPatchDown() {
+	yegCurrY = yegCurrY + MAP_DISP_HEIGHT;
+	yegCurrY = constrain(yegCurrY, 0, MAP_HEIGHT - MAP_DISP_HEIGHT);
+	lcd_image_draw(&yegImage, &tft, 
+				   yegCurrX, yegCurrY,
+				   0, 0,
+				   MAP_DISP_WIDTH, MAP_DISP_HEIGHT);
+}
+
+void drawNextPatchUp() {
+	yegCurrY = yegCurrY - MAP_DISP_HEIGHT;
+	yegCurrY = constrain(yegCurrY, 0, MAP_HEIGHT - MAP_DISP_HEIGHT);
+	lcd_image_draw(&yegImage, &tft, 
+				   yegCurrX, yegCurrY,
+				   0, 0,
+				   MAP_DISP_WIDTH, MAP_DISP_HEIGHT);
+}
+
+void joystickMode0() {
+    int xVal = analogRead(JOYSTICK_HORIZ);
+    int yVal = analogRead(JOYSTICK_VERT);
+    int buttonVal = digitalRead(JOYSTICK_SEL);
+
+    // stores the previous position of the cursor
+    prevX = cursorX;
+    prevY = cursorY;
+
+    // now move the cursor
+    if (yVal < (JOY_CENTER - JOY_DEADZONE)) {
+      if (yVal < (JOY_CENTER - JOY_DEADZONE - BUFFER)) {
+        cursorY -= 5;
+      }
+      cursorY -= 1; // decrease the y coordinate of the cursor
+    }
+    else if (yVal > JOY_CENTER + JOY_DEADZONE) {
+      if (yVal > (JOY_CENTER + JOY_DEADZONE + BUFFER)) {
+        cursorY += 5;
+      }
+      cursorY += 1;
+    }
+
+    // remember the x-reading increases as we push left
+    if (xVal > JOY_CENTER + JOY_DEADZONE) {
+      if (xVal > JOY_CENTER + JOY_DEADZONE + BUFFER) {
+        cursorX -= 5;
+      }
+      cursorX -= 1;
+    }
+    else if (xVal < JOY_CENTER - JOY_DEADZONE) {
+      if (xVal < JOY_CENTER - JOY_DEADZONE - BUFFER) {
+        cursorX += 5;
+      }
+      cursorX += 1;
+    }
+
+    // constrains cursor position to within the map patch displayed
+    cursorX = constrain(cursorX, CURSOR_SIZE/2, (DISPLAY_WIDTH - 61 - CURSOR_SIZE/2));
+    cursorY = constrain(cursorY, CURSOR_SIZE/2, (DISPLAY_HEIGHT - CURSOR_SIZE/2));
+
+    if (cursorX == (MAP_DISP_WIDTH - 1 - CURSOR_SIZE/2) && yegCurrX < (MAP_WIDTH - MAP_DISP_WIDTH)) {
+    	drawNextPatchRight();
+        // initial cursor position is the middle of the screen
+    	cursorX = MAP_DISP_WIDTH/2;
+    	cursorY = MAP_DISP_HEIGHT/2;
+    } else if (cursorX == (CURSOR_SIZE/2) && yegCurrX > 0) {
+    	drawNextPatchLeft();
+    	// inital cursor in middle of screen
+    	cursorX = MAP_DISP_WIDTH/2;
+    	cursorY = MAP_DISP_HEIGHT/2;
+    } else if (cursorY == (CURSOR_SIZE/2) && yegCurrY > 0) {
+    	drawNextPatchUp();
+    	// initial cursor in middle of screen
+    	cursorX = MAP_DISP_WIDTH/2;
+    	cursorY = MAP_DISP_HEIGHT/2;
+    } else if (cursorY == (DISPLAY_HEIGHT - CURSOR_SIZE/2) && yegCurrY < (MAP_HEIGHT - MAP_DISP_HEIGHT)) {
+    	drawNextPatchDown();
+    	// initial cursor
+    	cursorX = MAP_DISP_WIDTH/2;
+    	cursorY = MAP_DISP_HEIGHT/2;
+    }
+	// only redraws patch of Edmonton map if cursor position has changed
+	// this is to prevent "flickering"
+	if (prevX != cursorX or prevY != cursorY) {
+	  redrawMap();
+	}
+
+	// draw new cursor at new position
+	redrawCursor(TFT_RED);
+    delay(20);
+}
+
+// draw patch of screen for restaurant
+void selectedRestPatch() {
+	// if the x coordinate of the restaurant is >210px from either edge and
+	// y coordinate is more that 160 px away from top and bottom, 
+	// implement the cursor being at the location of the restaurant, both of which display
+	// at the center of the screen (i.e. take lat-mapdisplayheight/2 and long - mapdisplayheight/2)
+
+	// if the x coordinate is within 210 px from either edgeand
+	// y coordinate is more than 160 px away from top and bottom
+	//implement cursor at location of restaurant but map patch drawn constrained to 
+	// mapheight - mapdisplay height, and mapwidth - mapdisplaywidth
+
+	// if not in range of map,
+	// put cursor on closest location to restaurant. if it is outside x constraint, keep the
+	// latitude and put cursor on edge of screen, vice versa.
+}
+void mode0() {
+	// clear screen
+	tft.fillScreen(TFT_BLACK);
+    // draws the centre of the Edmonton map, leaving the rightmost 60 columns black
+    lcd_image_draw(&yegImage, &tft, 
+    			   yegCurrX, yegCurrY,
+                   0, 0,
+                   MAP_DISP_WIDTH, MAP_DISP_HEIGHT);
+
+    // initial cursor position is the middle of the screen
+    cursorX = MAP_DISP_WIDTH/2;
+    cursorY = MAP_DISP_HEIGHT/2;
+
+    redrawCursor(TFT_RED);
+    while (digitalRead(JOYSTICK_SEL) == HIGH) {
+    	joystickMode0();
+    }
+    selectedRest = 0;
+    mode1();
+}
+
+int main() {
+	setup();
+	// display list of restaurants that are closest to the chosen position
+		// 21 restaurants
+		// text size of 2
+		// sort by Manhattan distance
+		// d((x1,y1),(x2,y2)) = |x1 - x2| + |y1 - y2|
+	// user can use joystick to choose a restaurant (highlight block)
+	// when user pushes the button again, the program will go back to mode 0
+	// but with the selected restaurant at the centre
+	// IMPLEMENT THIS CODE WHEN JOYSTICK BUTTON IS PRESSED
+	// xPos, yPos = position of cursor when pressed
+	tft.setTextWrap(false);
+	tft.setTextSize(2);
+	while (true) {
+		mode0();
+	}
+	// when the user presses the joystick, enter mode 1
 	// replace the following with code that would display map
 	// etc, just filled screen for testing the joystick button
-	tft.fillScreen(TFT_BLACK);
+
 	Serial.end();
 
 	return 0;
